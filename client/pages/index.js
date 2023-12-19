c12d445aba5354b70c3363189b67d7e6c91910de
csarmientobaca <110415586+csarmientobaca@users.noreply.github.com>
2023-12-19 19:11:55 +0100

    fix indent

--- client/pages/index.js
+++ client/pages/index.js
@@ -16,7 +16,6 @@ import { TbWorldLatitude } from "react-icons/tb";
 
 import {
   Card,
-  CardContent,
   CardDescription,
   CardFooter,
   CardHeader,
@@ -73,98 +72,98 @@ query GetWeather($lat: Float!, $lon: Float!) {
 
 ///////function starts///////
 export default function index() {
-  const { loading: loadingCoords, error: errorCoords, data: dataCoords } = useQuery(GET_COORDS, {
-    variables: { lat: 12, lon: 33 }
-  });
+    const {loading: loadingCoords, error: errorCoords, data: dataCoords} = useQuery(GET_COORDS, {
+        variables: {lat: 12, lon: 33}
+    });
 
-  const { loading: loadingWeather, error: errorWeather, data: dataWeather } = useQuery(GET_WEATHER, {
-    variables: { lat: 33, lon: 33 }
-  });
+    const {loading: loadingWeather, error: errorWeather, data: dataWeather} = useQuery(GET_WEATHER, {
+        variables: {lat: 33, lon: 33}
+    });
 
 
-  if (loadingCoords || loadingWeather) return <p>Loading...</p>;
+    if (loadingCoords || loadingWeather) return <p>Loading...</p>;
 
-  if (errorCoords || errorWeather) {
+    if (errorCoords || errorWeather) {
+        return (
+            <p>
+                Error: {errorCoords ? errorCoords.message : errorWeather.message}
+            </p>
+        );
+    }
+
+    const {coord} = dataCoords.getWeather;
+    const {weather} = dataWeather.getWeather;
+
+    //defining the default values of the form inputs
+    const form = useForm({
+        resolver: zodResolver(formSchema),
+        defaultValues: {
+            lat: "",
+            lon: "",
+        },
+    });
+
+
     return (
-      <p>
-        Error: {errorCoords ? errorCoords.message : errorWeather.message}
-      </p>
-    );
-  }
+        <>
+            <h1>
+                Hello world coordinates:
+                <br/>lat {coord.lat}
+                <br/>lon {coord.lon}
+            </h1>
 
-  const { coord } = dataCoords.getWeather;
-  const { weather } = dataWeather.getWeather;
-
-//   //defining the default values of the form inputs
-  const form = useForm({
-    resolver: zodResolver(formSchema),
-    defaultValues: {
-      lat: "",
-      lon: "",
-    },
-  });
-
-
-  return (
-    <>
-      <h1>
-        Hello world coordinates:
-        <br />lat {coord.lat}
-        <br />lon {coord.lon}
-      </h1>
-
-      <h1>
-        Hello world weather:
-        {weather.map((w) => (
-          <div key={w.id}>
-            <br />id {w.id}
-            <br />icon {w.icon}
-          </div>
-        ))}
-      </h1>
-      <Card className="space-y-4 w-80 text-center bg-blue-100">
-        <CardHeader className="flex items-center justify-center flex-col">
-                        <CardTitle>{weatherobj.name.trim() !== "" ? weatherobj.name : "nothing/no city"}</CardTitle>
-
-                   {/* in this avatar the link is an image that the API provides too */}
-                       <Avatar className="mt-2">
-                          <AvatarImage src={`https://openweathermap.org/img/wn/${weatherobj.weather[0].icon}@2x.png`} />
+            <h1>
+                Hello world weather:
+                {weather.map((w) => (
+                        <div key={w.id}>
+                            <br/>id {w.id}
+                            <br/>icon {w.icon}
+                        </div>
+                    )
+                )
+                }
+            </h1>
+            <Card className="space-y-4 w-80 text-center bg-blue-100">
+                <CardHeader className="flex items-center justify-center flex-col">
+                    <CardTitle>
+                        {weatherobj.name.trim() !== "" ? weatherobj.name : "nothing/no city"}
+                    </CardTitle>
+                    {/* in this avatar the link is an image that the API provides too */}
+                    <Avatar className="mt-2">
+                        <AvatarImage src={`https://openweathermap.org/img/wn/${weatherobj.weather[0].icon}@2x.png`}/>
                         <AvatarFallback>weatherIcon</AvatarFallback>
-                         </Avatar>
-                      <CardDescription>
-                     </CardDescription>
-                         <CardDescription className="flex items-center">
-                           <TbWorldLatitude />
-                           <p className="ml-1">Latitude: {weatherobj.coord.lat}</p>
-                           <TbWorldLongitude className="ml-2" />
-                           <p className="ml-1">Longitude: {weatherobj.coord.lon}</p>
-                         </CardDescription>
-                       </CardHeader>
-
-                       <CardFooter className="flex items-center justify-center">
-                         <p>
-                           {weatherobj.main.temp}
-                         </p>
-                         <TbTemperatureCelsius />
-                       </CardFooter>
-                       <CardFooter className="flex items-center justify-center">
-                         <FaTemperatureArrowDown />
-                         <p>
-                           MIN {weatherobj.main.temp_min}
-                         </p>
-                         <TbTemperatureCelsius />
-                       </CardFooter>
-                       <CardFooter className="flex items-center justify-center">
-                         <FaTemperatureArrowUp />
-                         <p>
-                           MAX {weatherobj.main.temp_max}
-                         </p>
-                         <TbTemperatureCelsius />
-                       </CardFooter>
-                     </Card>
-    </>
-  )
+                    </Avatar>
+                    <CardDescription className="flex items-center">
+                        <TbWorldLatitude/>
+                        <p className="ml-1">Latitude: {weatherobj.coord.lat}</p>
+                        <TbWorldLongitude className="ml-2"/>
+                        <p className="ml-1">Longitude: {weatherobj.coord.lon}</p>
+                    </CardDescription>
+                </CardHeader>
+                <CardFooter className="flex items-center justify-center">
+                    <p>
+                        {weatherobj.main.temp}
+                    </p>
+                    <TbTemperatureCelsius/>
+                </CardFooter>
+                <CardFooter className="flex items-center justify-center">
+                    <FaTemperatureArrowDown/>
+                    <p>MIN {weatherobj.main.temp_min}
+                    </p>
+                    <TbTemperatureCelsius/>
+                </CardFooter>
+                <CardFooter className="flex items-center justify-center">
+                    <FaTemperatureArrowUp/>
+                    <p>
+                        MAX {weatherobj.main.temp_max}
+                    </p>
+                    <TbTemperatureCelsius/>
+                </CardFooter>
+            </Card>
+        </>
+    )
 }
+
 
 //   const [weatherobj, setWeatherobj] = useState({})
 
@@ -243,6 +242,7 @@ export default function index() {
 //           </Form>
 //         )}
 //       </div>
+//   )}
 //     </>
 //   );
 // }
